24ebbce57a6aa3b28416039b90d50f83ee9a9532
Prasad Bandagale <120239484+Prasad-b-git@users.noreply.github.com>
2025-04-11 17:11:12 +0530

    Update FeaturedProjects.js

--- src/components/home/FeaturedProjects.js
+++ src/components/home/FeaturedProjects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+//import { motion } from 'framer-motion';
+//import { Link } from 'react-router-dom';
 import projects from '../../data/projects';
 import ProjectCard from '../projects/ProjectCard';
 
